aae5d6c70887c1d24d96c9dd4e8e5c860a458259
lincy sam <lincysam@gmail.com>
2023-11-07 12:11:40 +0000

    Third commit

--- smartphone/views.py
+++ smartphone/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 import datetime,time
 from .models import *
 from datetime import *
+from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.db.models import Q
 # Create your views here.
 def homepage(request):
     return render(request,'mobilecenter.html')
@@ -12,17 +14,26 @@ def brandpage(request):
     
 def createbrand(request):
     if request.method =='POST':
-        
-        brandname=request.POST['brandname']
-        brandfile=request.FILES['myfile']
-        now = datetime.now()
-        
-        savedata=brand(brandname=brandname,created_at=now,updated_at=now,brand_image=brandfile)
-        savedata.save()
-        msg="Data saved"
-        return render(request,'mobilecenter.html')
+        try:   
+            brandname=request.POST['brandname']
+            brandfile=request.FILES['myfile']
+            now = datetime.now()
+            if(brandname =="" or brandfile==""):
+                
+                msg="Please fill required fields"
+                return render(request,'brand.html',{'msg':msg})
+            else:
+                savedata=brand(brandname=brandname,created_at=now,updated_at=now,brand_image=brandfile)
+                savedata.save()
+                msg="Data saved"
+                return render(request,'mobilecenter.html')
+        except Exception as e:
+            print("Some error occured")       
+                 
+                 
 def modelpage(request):
     brandobj=brand.objects.all()
+    
     return render(request,'model.html',{'brandobj':brandobj})   
 
 def  createmodel(request):
@@ -35,7 +46,8 @@ def  createmodel(request):
         available=request.POST['available']
         mobile_image=request.FILES['myfile']
         now = datetime.now()
-        
+        if (int(price) < 0):
+            raise ValueError("Work with positive numbers only")
         bid=brand(id=brand_id)
         savedata=phonemodel(brand_id=bid,created_at=now,updated_at=now,mobile_image=mobile_image,modelname=modelname,release_year=release_year,available_quantities=quantity,price=price,is_available=available)
         savedata.save()
@@ -47,23 +59,37 @@ def brandlist(request):
         return render(request,'listbrand.html',{'brandobj': brandobj})
 def modellist(request):
         brand_id=request.GET['brand_id']
-        modelobj=phonemodel.objects.filter(brand_id=brand_id)
+        modelobj=phonemodel.objects.filter(brand_id=brand_id) 
+       
         return render(request,'listmodel.html',{'modelobj': modelobj})
+        
+
 def transaction(request):
         model_id=request.GET['model_id']
         
         modelobj=phonemodel.objects.get(id=model_id)
-        print("Name:",modelobj)
+        
+        
         return render(request,'trans.html',{'modelobj': modelobj})
+        
+            
 def payment(request):
     if request.method=='POST':
-        trans_model=request.POST['model_id']
-        modelobj=phonemodel.objects.get(id=trans_model)
-        userdata = User.objects.get(username=request.user)
-        model_id=phonemodel(id=trans_model)
-        transaction_type=request.POST['transtype']
-        t_amount=modelobj.price
-        savedata=mobile_trans(trans_model=model_id,transaction_type=transaction_type,t_amount=t_amount,usersample=userdata)
-        savedata.save()
-        msg="You have completed your transaction successfully"
-        return render(request,'success.html',{'msg':msg})
+        try:
+            trans_model=request.POST['model_id']
+            
+            modelobj=phonemodel.objects.get(id=trans_model)
+            
+            userdata = User.objects.get(username=request.user)
+            model_id=phonemodel(id=trans_model)
+            transaction_type=request.POST['transtype']
+            t_amount=modelobj.price
+            savedata=mobile_trans(trans_model=model_id,transaction_type=transaction_type,t_amount=t_amount,usersample=userdata)
+            savedata.save()
+            modelobj.available_quantities -=1
+            modelobj.updated_at=datetime.now()
+            modelobj.save()
+            msg="You have completed your transaction successfully"
+            return render(request,'success.html',{'msg':msg})
+        except Exception as e:
+            print("Some Error occured")
